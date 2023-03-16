d3c8440d617a88778fb825159c829ede09adb98a
doonee <doonee@outlook.com>
2023-03-16 16:32:07 +0900

    added easy-peasy

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
 import { Routes, Route, Outlet } from 'react-router-dom';
-import { DataProvider } from './context/DataContext';
+// import { DataProvider } from './context/DataContext-backup';
+import { useEffect } from 'react';
+import useAxiosFetch from './hooks/useAxiosFetch';
+import { useStoreActions } from 'easy-peasy';
 
 function App() {
+  const setPosts = useStoreActions(actions => actions.setPosts)
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data, setPosts])
+
   const Layout = () => {
     return (
       <div className="App">
@@ -25,18 +35,21 @@ function App() {
   };
 
   return (
-    <DataProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="post" element={<NewPost />} />
-          <Route path="edit/:id" element={<EditPost />} />
-          <Route path="post/:id" element={<PostPage />} />
-          <Route path="*" element={<Missing />} />
-        </Route>
-      </Routes>
-    </DataProvider>
+    // <DataProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home
+          isLoading={isLoading}
+          fetchError={fetchError}
+        />} />
+        <Route path="about" element={<About />} />
+        <Route path="post" element={<NewPost />} />
+        <Route path="edit/:id" element={<EditPost />} />
+        <Route path="post/:id" element={<PostPage />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+    // </DataProvider>
   );
 }
 
--- src/NewPost.js
+++ src/NewPost.js
@@ -1,47 +1,43 @@
-import { useContext, useState } from 'react';
-import DataContext from './context/DataContext';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import { format } from 'date-fns';
-import api from './api/posts';
+import { useStoreState, useStoreActions } from 'easy-peasy';
 
 const NewPost = () => {
-  const [postTitle, setPostTitle] = useState('');
-  const [postBody, setPostBody] = useState('');
-  const { posts, setPosts } = useContext(DataContext);
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
+  const posts = useStoreState((state) => state.posts);
+  const postTitle = useStoreState((state) => state.postTitle);
+  const postBody = useStoreState((state) => state.postBody);
+
+  const savePost = useStoreActions((actions) => actions.savePost);
+  const setPostTitle = useStoreActions((actions) => actions.setPostTitle);
+  const setPostBody = useStoreActions((actions) => actions.setPostBody);
+
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    try {
-      const response = await api.post('/posts', newPost);
-      const allPosts = [...posts, response.data];
-      setPosts(allPosts);
-      setPostTitle('');
-      setPostBody('');
-      navigate('/');
-    } catch (err) {
-      console.log(err);
-    }
+    savePost(newPost);
+    navigate('/');
   }
 
   return (
     <main className="NewPost">
       <h2>New Post</h2>
       <form className="newPostForm" onSubmit={handleSubmit}>
-        <label htmlFor="postTitle1">Title:</label>
+        <label htmlFor="postTitle">Title:</label>
         <input
-          id="postTitle1"
+          id="postTitle"
           type="text"
           required
           value={postTitle}
           onChange={(e) => setPostTitle(e.target.value)}
         />
-        <label htmlFor="postBody1">Post:</label>
+        <label htmlFor="postBody">Post:</label>
         <textarea
-          id="postBody1"
+          id="postBody"
           required
           value={postBody}
           onChange={(e) => setPostBody(e.target.value)}
