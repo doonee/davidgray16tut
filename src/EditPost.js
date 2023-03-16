3254d713381020c8c3fb8e13f5dbde2cf6e63f39
doonee <doonee@outlook.com>
2023-03-16 09:59:26 +0900

    added usecontext and dataprovider

--- src/EditPost.js
+++ src/EditPost.js
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import DataContext from './context/DataContext';
+import { useNavigate } from 'react-router-dom'
+import { format } from 'date-fns';
+import api from './api/posts';
 
-const EditPost = ({
-  posts, handleEdit, editBody, setEditBody, editTitle, setEditTitle
-}) => {
+const EditPost = () => {
+  const { posts, setPosts } = useContext(DataContext)
   const { id } = useParams();
   const post = posts.find(post => (post.id).toString() === id);
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (post) {
@@ -13,6 +19,20 @@ const EditPost = ({
       setEditBody(post.body);
     }
   }, [post, setEditTitle, setEditBody])
+
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
+      setEditTitle('');
+      setEditBody('');
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
 
   return (
     <main className="NewPost">
